f18c41d9c8ee6f0b9cf40e98527810269155203e
Gomgom Immanuel <68519252+gomgomimmanuel@users.noreply.github.com>
2021-11-03 18:12:47 +0700

    Add files via upload

---
+++ showdata.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//tipe data
+	/* 
+	by Gomgom Immanuel
+	gomgomimmanuel@live.com
+	you can call me Noel...
+	*/
+	string my_name = "Gomgom Immanuel";
+	int int_number = 98;
+	float float_number = 98.1;
+	double double_number = 98.2; 
+	char character = 'N';
+	bool boolean = true;
+
+	cout<<"string my_name : "<<my_name<<endl;
+	cout<<"int number : "<<int_number<<endl;
+	cout<<"float number : "<<float_number<<endl;
+	cout<<"double number : "<<double_number<<endl;
+	cout<<"char : "<<character<<endl;
+	cout<<"boolean "<<boolean<<endl;
+	
+}
---
+++ tipedata.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"8 + 5"<<endl;
+	cout<<8 + 5<<endl;
+}
